e2b228545c65c770066d28a01860e8587593f604
alfdkfenfpgkrry0505 <51285169+alfdkfenfpgkrry0505@users.noreply.github.com>
2019-06-27 20:04:28 +0900

    Update py.py

--- py.py
+++ py.py
@@ -69,12 +69,12 @@ async def on_message(message):
 @app.event
 async def my_background_task():
     await app.wait_until_ready()
-    channel = discord.Object(id='CHANNEL_ID')
+    channel = discord.Object(id='585087748952817665')
     while not app.is_closed:
         await app.channel.send("용병님들! 요즘 뭐 바쁜 일 있으세요?")
         await app.channel.send("지금 배틀그라운드 모바일 접속하시면 좋은 일이 생길거에요~!!")
         await app.channel.send("행복한 하루 되세요~ ^00^ ")
-        await asyncio.sleep(1) 
+        await asyncio.sleep(604800) 
 
 @app.event
 async def on_member_join(member):
