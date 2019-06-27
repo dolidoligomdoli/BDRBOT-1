78ef636a24a1aa3916b4d20c82ab1d46eb3ec20b
alfdkfenfpgkrry0505 <51285169+alfdkfenfpgkrry0505@users.noreply.github.com>
2019-06-27 18:37:55 +0900

    Update py.py

--- py.py
+++ py.py
@@ -37,8 +37,8 @@ async def on_message(message):
         await message.channel.send("컴맹들을 위한 천국, 컴퓨존이지!")
     if message.content.startswith("!커뮤니티 웹사이트 추천"):
         await message.channel.send("***TOP 3***")
-        await message.channel.send("1위는 디시인사이드 입니다!")
-        await message.channel.send("2위는 Superopennet 입니다!")
+        await message.channel.send("1위는 네덕 급식들이 판치는 네이버 카페 입니다!")
+        await message.channel.send("2위는 대한민국에서 현존하는 제일 큰 커뮤니티인 디시인사이드 입니다!")
         await message.channel.send("3위는 대한민국에서 다소 부정적인 인식을 가지고 있는 게임 커뮤니티, 인벤 입니다!")
         await message.channel.send("위 리스트에 올라온 커뮤니티 웹사이트들은 그나마 클-린한 웹사이트만을 엄격히 선별하여 골랐습니다.")
     if message.content.startswith("!볼 만한 유튜버 추천"):
@@ -47,15 +47,15 @@ async def on_message(message):
         await message.channel.send("3위는 iT 관련 제품들을 리뷰해주시거나 그 관련 기능들을 자세히 설명해주시는 유튜버 잇섭님 입니다")
     if message.content.startswith("!모바일 게임 추천"):
         await message.channel.send("***TOP 3***")
-        await message.channel.send("1위는 배틀그라운드 모바일 입니다")
-        await message.channel.send("2위는 하루종일 이라도 게임 할 수 있어!, 마블 퓨처파이트 입니다.")
-        await message.channel.send("3위는 펜타스톰 입니다")
+        await message.channel.send("1위는 클래시 오브 클랜 입니다")
+        await message.channel.send("2위는 이제 모바일에서도 배틀그라운드!, 배틀그라운드 모바일 입니다")
+        await message.channel.send("3위는 없습니다;;")
     if message.content.startswith("!자기소개"):
         await message.channel.send("안녕? 난 배돌이란다.")
         await message.channel.send("너희들이 디스코드 서버를 잘 이용할 수 있도록 ")
-        await message.channel.send("뒤에서 도와주는 디스코드 PUBG モバイル 서버 한정 어시스턴트야.")
-        await message.channel.send("내가 주 업무를 맡는 시간대는 주로 금요일부터 일요일 까지란다.")
-        await message.channel.send("그런데, 그것도 나의 보스가 허락하지 않는다면 수행하지도 못해..")
+        await message.channel.send("뒤에서 도와주는 디스코드 PUBG MOBILE GLOBAL 서버 한정 어시스턴트야.")
+        await message.channel.send("내가 주 업무를 맡는 시간대는 주로 금요일부터 일요일 였으나 2019년 6월 27일 드디어 월요일부터 일요일까지 업무를 맡을 수 있게되었어..")
+        await message.channel.send("그런데, 그것도 나의 보스가 호스팅을 끊어버린다면 제대로 할수가 없게 되지....")
         await message.channel.send("============")
         await message.channel.send("============")
         await message.channel.send("============")
