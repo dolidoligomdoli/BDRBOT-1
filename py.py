56bf300260101532de1e632c717dbeea6f18432d
alfdkfenfpgkrry0505 <51285169+alfdkfenfpgkrry0505@users.noreply.github.com>
2019-06-27 20:43:40 +0900

    Update py.py

--- py.py
+++ py.py
@@ -98,36 +98,8 @@ async def on_member_join(member):
     await app.message.channel.send(member, "내가 알려줄 수 있는 범위 안에서 최선을 다해 알려줄테니까.")
     await app.message.channel.send(member, "아 참! 우리 서버에 들어온 것을 환영해~")
 
-@app.event
-bot = commands.Bot(command_prefix='!!')
- 
-async def func_comeon(NC):
-    voice = bot.voice_client_in(NC.message.server)
-    if NC.message.author.voice.voice_channel:
-        if voice:
-            if voice.channel !!= NC.message.author.voice.voice_channel:
-                print('초대할 서버로 초대하는 중...')
-                await voice.move_to(NC.message.author.voice.voice_channel)
-        else:
-            print('널에 합류시키는 중...')
-            voice = await bot.join_voice_channel(NC.message.author.voice.voice_channel)
-    else:
-        await bot.say('ㅠㅠ너 진짜 어디에 있니??')
-        print('머무르는 중...')
-    return voice
- 
-@bot.command(pass_context=True)
-async def comeon(NC):
-    await func_comeon(NC)
- 
-@bot.command(pass_context=True)
-async def play(NC):
-    voice = await func_comeon(NC)
-    player = voice.create_ffmpeg_player('music.mp3')
-    player.start()
 
-bot.run(accross_token)
 
-app.loop.create_task(my_background_task())
 accross_token = os.environ["BOT_TOKEN"]
+app.loop.create_task(my_background_task())
 app.run(accross_token)
