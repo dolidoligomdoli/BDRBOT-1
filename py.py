d0ef0ad84df71341703624a137cb16d86b8414d3
alfdkfenfpgkrry0505 <51285169+alfdkfenfpgkrry0505@users.noreply.github.com>
2019-06-30 00:05:28 +0900

    Update py.py

--- py.py
+++ py.py
@@ -63,7 +63,7 @@ async def on_message(message):
         await message.channel.send(embed=embed)
         embed = discord.Embed(title=" !PC 게임 추천 ", description=" 배돌이가 현재 흥행하는 PC 게임 3개를 추천해줍니다", color=0xff0000)
         await message.channel.send(embed=embed)
-        embed = discord.Embed(title=" !반언어 ", description=" 배돌이가 무슨 언어를 기반으로 개발되어지고 있는지 알려줍니다 ", color=0xff0000)
+        embed = discord.Embed(title=" !기반언어 ", description=" 배돌이가 무슨 언어를 기반으로 개발되어지고 있는지 알려줍니다 ", color=0xff0000)
         await message.channel.send(embed=embed)
         embed = discord.Embed(title=" !패치노트 ", description=" 배돌이가 자신의 패치노트를 불러옵니다 ", color=0xff0000)
         await message.channel.send(embed=embed)
