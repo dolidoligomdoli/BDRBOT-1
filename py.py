b1255aa8b4e9f67aeec18548d06c021c14783659
alfdkfenfpgkrry0505 <51285169+alfdkfenfpgkrry0505@users.noreply.github.com>
2019-06-29 23:27:14 +0900

    Update py.py

--- py.py
+++ py.py
@@ -157,7 +157,7 @@ async def on_message(message):
         await message.channel.send(embed=embed)
         
     if message.content.startswith("!공지"):
-        embed = discord.Embed(title=" 공지사항 ", description=" ", color=0xffaaaa)
+        embed = discord.Embed(title=" 공지사항 ", description="@ ", color=0xffaaaa)
         await message.channel.send(embed=embed)
         embed = discord.Embed(title=" ", description=" 배틀그라운드 모바일을 멀리하지 마세요... ", color=0xffaaaa)
         await message.channel.send(embed=embed)  
@@ -165,7 +165,7 @@ async def on_message(message):
         await message.channel.send(embed=embed)
         embed = discord.Embed(title="  ", description=" 당신들은 스트레스 받으면서 살기에는 너무 아깝고 귀한 존재에요... ", color=0xffaaaa)
         await message.channel.send(embed=embed)
-        embed = discord.Embed(title=" 2019.06.29.토.23:26 ", description=" ", color=0xffaaaa)
+        embed = discord.Embed(title=" 2019.06.29.토.23:28 ", description=" ", color=0xffaaaa)
         await message.channel.send(embed=embed)
         
 
