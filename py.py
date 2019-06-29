e0f337bbdaddabc9712b69dba131a6471bf713e0
alfdkfenfpgkrry0505 <51285169+alfdkfenfpgkrry0505@users.noreply.github.com>
2019-06-30 00:05:09 +0900

    Update py.py

--- py.py
+++ py.py
@@ -16,7 +16,7 @@ async def on_ready():
     
 @app.event
 async def on_member_join(member):
-    fmt = '{1.name} 에 오신것을 환영합니다., {0.mention} 님'
+    fmt = '{1.name} 에 온 것을 진심으로 환영해, {0.mention} 님'
     channel = member.server.get_channel("585087748952817665")
     await app.message.channel.send( fmt.format(member, member.server))
     await app.message.channel.send(member, "안녕? 난 뉴 배돌이야.")
@@ -27,7 +27,7 @@ async def on_member_join(member):
 @app.event
 async def on_member_remove(member):
     channel = member.server.get_channel("585087748952817665")
-    fmt = '{0.mention} 님이 서버에서 나가셨습니다.'
+    fmt = '{0.mention} ... 야, 왜 나가냐 씨발아'
     await app.message.channel.send( fmt.format(member, member.server))
           
 @app.event
@@ -63,7 +63,7 @@ async def on_message(message):
         await message.channel.send(embed=embed)
         embed = discord.Embed(title=" !PC 게임 추천 ", description=" 배돌이가 현재 흥행하는 PC 게임 3개를 추천해줍니다", color=0xff0000)
         await message.channel.send(embed=embed)
-        embed = discord.Embed(title=" !언어 ", description=" 배돌이가 무슨 언어를 기반으로 개발되어지고 있는지 알려줍니다 ", color=0xff0000)
+        embed = discord.Embed(title=" !반언어 ", description=" 배돌이가 무슨 언어를 기반으로 개발되어지고 있는지 알려줍니다 ", color=0xff0000)
         await message.channel.send(embed=embed)
         embed = discord.Embed(title=" !패치노트 ", description=" 배돌이가 자신의 패치노트를 불러옵니다 ", color=0xff0000)
         await message.channel.send(embed=embed)
