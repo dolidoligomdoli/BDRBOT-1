31fee8db0025db4b946688b382b152be7c5da525
alfdkfenfpgkrry0505 <51285169+alfdkfenfpgkrry0505@users.noreply.github.com>
2019-06-30 19:10:41 +0900

    Update py.py

--- py.py
+++ py.py
@@ -211,6 +211,30 @@ async def on_message(message):
             await message.channel.send( embed=discord.Embed(description=':game_die: ' + ':six: ',color=0xfefe00))
         
 
+    if message.content.startswith('!네이버실검'):
+        url = "https://www.naver.com/"
+        html = urllib.request.urlopen(url)
+
+        bsObj = bs4.BeautifulSoup(html, "html.parser")
+        realTimeSerach1 = bsObj.find('div', {'class': 'ah_roll_area PM_CL_realtimeKeyword_rolling'})
+        realTimeSerach2 = realTimeSerach1.find('ul', {'class': 'ah_l'})
+        realTimeSerach3 = realTimeSerach2.find_all('li')
+
+
+        embed = discord.Embed(
+            title='실시간 검색어',
+            description='',
+            color=discord.Color.green()
+        )
+        for i in range(0,20):
+            realTimeSerach4 = realTimeSerach3[i]
+            realTimeSerach5 = realTimeSerach4.find('span', {'class': 'ah_k'})
+            realTimeSerach = realTimeSerach5.text.replace(' ', '')
+            realURL = 'https://search.naver.com/search.naver?ie=utf8&query='+realTimeSerach
+            print(realTimeSerach)
+            embed.add_field(name=str(i+1)+'위', value='\n'+'[%s](<%s>)' % (realTimeSerach, realURL), inline=False) 
+
+        await ssage.channel.send(embed=embed)
     
 
 
