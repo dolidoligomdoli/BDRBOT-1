88425b47b8011c3e8e354999cb3e849141b0dfd2
alfdkfenfpgkrry0505 <51285169+alfdkfenfpgkrry0505@users.noreply.github.com>
2019-07-05 20:00:42 +0900

    Update py.py

--- py.py
+++ py.py
@@ -208,7 +208,7 @@ async def on_message(message):
         await message.channel.send(embed=embed)
         embed = discord.Embed(title=" *0.0.9 ", description=" 2019년 07월 05일 금요일, 일부 Ui 수정!", color=0x00fefe)
         await message.channel.send(embed=embed)
-        embed = discord.Embed(title=" *0.1.0 ", description=" 2019년 07월 05일 금요일, 이제 배돌이가 너희에게 하고싶은 말들을 보내줄 수 있단다!", color=0x00fefe)
+        embed = discord.Embed(title=" *0.1.0 ", description=" 2019년 07월 05일 금요일, !내쪽지 기능 업데이트", color=0x00fefe)
         await message.channel.send(embed=embed)
         embed = discord.Embed(title=" ", description=" ***패치노트는 계속 업데이트 할 예정입니다 ^00^ ", color=0x00fefe)
         await message.channel.send(embed=embed)
